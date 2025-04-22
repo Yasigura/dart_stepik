ced73dad7c6ed43ebb42529399919a63a9ce6691
Yasigura <lyguska228@gmail.com>
2025-04-22 23:12:40 +0300

    lesson 2.8

--- lib/dart_stepik.dart
+++ lib/dart_stepik.dart
@@ -3,16 +3,8 @@ import 'dart:io';
 void main() {
   var forceA = stdin.readLineSync() ?? "0";
   var forceB = stdin.readLineSync() ?? "0";
-  int a = int.parse(forceA) ?? 0;
-  int b = int.parse(forceB) ?? 0;
-  if (a == b) {
-    int c = a + b;
-    int d = c + a;
-    stdout.writeln(c);
-    stdout.writeln(d);
-  } else {
-    int c = a + b;
-    stdout.writeln(c);
-    stdout.writeln(c);
-  }
+  int a = int.tryParse(forceA) ?? 0;
+  int b = int.tryParse(forceB) ?? 0;
+  int c = a * b;
+  stdout.write("Площадь помещения: $c");
 }
