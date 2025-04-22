8965ba017b422c335a2e62a89f7e77a000b36dff
Yasigura <lyguska228@gmail.com>
2025-04-22 22:18:44 +0300

    lesson 2.7

--- lib/dart_stepik.dart
+++ lib/dart_stepik.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
+
 void main() {
-  print("Hello world");
+  final a = stdin.readLineSync() ?? "0";
+  final b = stdin.readLineSync() ?? "0";
+  int? cifA = int.tryParse(a);
+  int? cifB = int.tryParse(b);
+
+  if (cifA == null || cifB == null) {
+    stdout.write("Неправильное значение");
+    return;
+  }
+
+  print(cifA + cifB);
 }
